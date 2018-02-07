eef8af09911e3546dea836a3be9535f42ea7294c
Michael Muhlbaier <mm@spaghettiengineering.com>
2018-02-07 15:51:55 -0500

    Adds Timing, Task, and UART module headers with documentation.

---
+++ include/task.h
@@ -0,0 +1,210 @@
+#ifndef _TASK_H_
+#define _TASK_H_
+/** @file
+ * @addtogroup task Task Management Module
+ *
+ * @brief The Task Management Module implements a task queue and a task schedule
+ *
+ * The task queue is a single linked FIFO list.
+ * Each time SystemTick() is called the first task in the queue will execute.
+ * If the task is periodic it will be added back to the task schedule once it is ran.
+ *
+ * The task schedule is a single linked list of tasks in order of when they are
+ * scheduled to run. If two tasks are scheduled for the same time there is no guarantee
+ * which task will run first. Each task scheduled has a timestamp which
+ * indicates when it ran last and the period if it is to be run again.
+ * If the task was just added to the schedule then time ran will indicate the time it
+ * should first nun minus the period. When SystemTick() is called it will check if any
+ * scheduled tasks are due to run and add them to the task queue when they are
+ * due.
+ *
+ * UPDATED 10/4/2012
+ * Remove functions were not interrupt safe -> Fixed
+ *
+ * UPDATED 2/18/2014
+ * Fixed bug in linking multiple tasks at once and/or scheduling multiple tasks
+ * at once. Added extra checks to ensure task index is in range everytime the
+ * task array is accessed.
+ *
+ * UPDATED 3/1/2016
+ * Task_Remove for the task that is running was not working because the task
+ * was not linked in the list. Task_Remove was update to traverse the entire
+ * array of tasks instead of using List_GetItem.
+ *
+ * @{
+ */
+
+#include <stdint.h>
+#include "system.h"
+
+#ifndef USE_MODULE_TASK
+#warning "USE_MODULE_TASK not defined in system.h. Other modules won't be able to utilize this module."
+#endif
+
+#include "subsys.h"
+
+// Make sure timing.h is included
+#include "timing.h"
+
+/// helpful typedef's for casting functions if you want to avoid the incompatible
+/// pointers warning
+typedef void (*task_fn_pointer_input_t)(void *);
+/// default task type
+typedef void (*task_fn_t)(void);
+
+ /** The number of tasks that can be added to the task queue or task schedule or task trigger list
+ *
+ * Note: the task queue and task schedule list use the same array of tasks in
+ * implementing the linked lists. If the array becomes full the
+ * module will drop the task that is to be added.
+ *
+ */
+
+#ifndef MAX_TASK_LENGTH
+#define MAX_TASK_LENGTH 20
+#endif
+
+
+/** The time when the task management system will force the timer to roll over
+ *
+ * The task management module schedules tasks in the future by setting the time to
+ * run as some time in the future. Once the current time + the period exceeds TIME_MAX
+ * the task time to run will roll over and cause the task to run erroneously. Thus the
+ * task managemnent system must force the timing module's internal time to rollover
+ * prior to when it would natrually roll over (TIME_MAX). The default is 10 days
+ * prior to TIME_MAX which means the task managment system can only handle tasks
+ * scheduled within 10 days and no longer.
+ */
+#ifndef TASK_ROLL_TIME
+#define TASK_ROLL_TIME (TIME_MAX-(uint32_t)10*24*60*60*1000) // roll 10 days before the timer would naturally roll
+#endif
+
+/** Initialize Task Management Module
+ *
+ * Initializes the task management module for operation.
+ */
+void Task_Init(void);
+
+/** Run the first task in the task queue and check if any tasks are due to run
+ * in the schedule
+ *
+ * Run the first task in the task queue provided one exists. If the task ran has
+ * a period value then it will be moved to the task schedule instead of removed.
+ *
+ * Check if any of the scheduled tasks are due to run. If so move any tasks
+ * whose run time is at or before the current time to the task queue where they
+ * will be ordered by priority and executed accordingly.
+ *
+ * If the Event Module is included check for events
+ */
+void SystemTick(void);
+
+/**
+ * @brief Adds a function to the task queue.
+ *
+ * Task_Queue() adds a task to be run by the task management system to the queue.
+ * Use this function to add the function to be run
+ *
+ * @author David Calhoun
+ * @author Tony Samaritano
+ * @author Aaron Johnson
+ * @author Michael Muhlbaier
+ *
+ * @param fn Function Pointer - must have no return value, can have a pointer input or no input.
+ * If it has an input then you may want to cast it using (task_fn_pointer_input_t)
+ * to avoid compiler warning about
+ * @param pointer pointer to pass to the task when run. Set to 0 if the task has no input
+ */
+void Task_Queue(task_fn_t fn, void * pointer);
+
+/**
+ * @brief Adds task to be scheduled for execution
+ *
+ * This function adds a task that is scheduled for later time.
+ * Tasks are order strictly by their time (FIFO)
+ *
+ * Note: tasks are not guaranteed to run at the exact time specified by delay
+ * and period. Timing will depend on how often SystemTick(). In
+ * properly designed systems SystemTick() should be called frequently enough
+ * to keep the number of tasks in the queue low so scheduled tasks are run on
+ * time.
+ *
+ * @author Aaron Johnson
+ * @author David Calhoun
+ * @author Tony Samaritano
+ * @author Ryan Lee
+ * @author Michael Muhlbaier
+ *
+ * @param fn Function Pointer - must have no return value, can have a pointer input or no input.
+ * If it has an input then you may want to cast it using (task_fn_pointer_input_t)
+ * to avoid compiler warning about
+ * @param pointer pointer to pass to the task when run. Set to 0 if the task has no input
+ * @param delay Delay before the task is first run
+ * @param period Period of how often the task is run (0 no rescheduling)
+ */
+int8_t Task_Schedule(task_fn_t fn, void * pointer,
+        tint_t delay, tint_t period);
+
+/** @brief Sets a task to run whenever the task queue is empty / no tasks due to run
+ *
+ * @param fn Function Pointer - must have no return value and no input.
+ * @return 0 normally, 1 if a previous idle task was just overwritten
+ */
+int8_t Task_SetIdleTask(task_fn_t fn);
+
+
+/**
+ * @brief Removes Task
+ *
+ * RemoveTask() loops through the entire task management queue and schedule and 
+ * removes that task from the task management system.
+ *
+ * If the task could be in the list more than once then you should call Task_Remove
+ * as many times as you suspect the task may be in the list.
+ *
+ * @author Aaron Johnson
+ * @author David Calhoun
+ * @author Tony Samaritano
+ * @author Michael Muhlbaier
+ *
+ * @param fn Function Pointer - must have no return value, can have a pointer input or no input.
+ * If it has an input then you may want to cast it using (task_fn_pointer_input_t)
+ * to avoid compiler warning about
+ * @param pointer Input Pointer, if the function has no input pointer or if you
+ * want to remove all fn functions regardless of the pointer then make pointer 0
+ */
+void Task_Remove(task_fn_t fn, void * pointer);
+
+/** Wait a set number of milliseconds and run queued or scheduled tasks while
+ * waiting
+ *
+ * WaitMs is similar to DelayMs from Timer Module, exception being that it
+ * repeatedly calls SystemTick() to allow functions to be run from the Queue
+ * and the Schedule.
+ *
+ * @warning not safe to be called from any function that could be called by
+ * SystemTick() - suggest using a dedicated mutex/flag and only use WaitMs() in
+ * main or functions called by main (not through SystemTick()).
+ *
+ * @param wait  time amount for the wait
+ */
+void WaitMs(tint_t wait);
+
+/** Checks if a task is scheduled or queued
+ * 
+ * @param fn function to look for
+ * @return 1 if task is scheduled or queued, 0 if not
+ */
+uint8_t Task_IsScheduled(task_fn_t fn);
+
+/** Update the period of an existing task
+ * 
+ * @param fn pointer to the function whose task should be updated
+ * @param period new period
+ * @param update_next_time 0 - don't rescheule the next run, 1 reschedule the next run base on the new period
+ */
+void Task_ChangePeriod(task_fn_t fn, tint_t period, uint8_t update_next_time);
+
+/** @}*/
+
+#endif // _TASK_H_
---
+++ include/uart.h
@@ -0,0 +1,379 @@
+/**
+ * @defgroup uart UART Module
+ * @file uart.h
+ *
+ *  Created on: Mar 12, 2014
+ *  @author: Michael Muhlbaier
+ *  Updated on: Feb 7, 2015
+ *  @author Anthony Merlino
+ *
+ * Use this module for basic UART communications. Completely interrupt driven
+ * and buffered. 
+ *
+ * Normal usage is:
+ * - UART_Init() to initialize a UART channel.
+ * - Transmit data
+ * -- UART_WriteByte() to send a byte
+ * -- UART_Write() to send an array of bytes
+ * -- UART_Printf() to send a formatted string
+ * - UART_RegisterReceiver() to register a character receiver to process received data
+ * - Call UART_Tick() as frequently as possible (every 1ms or more). Note: this happens
+ * automatically if using @ref task with USE_MODULE_TASK defined in system.h
+ * @{
+ */
+
+#ifndef _UART_H_
+#define _UART_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+// Get library configuration
+#include "system.h"
+
+#ifndef USE_MODULE_UART
+#warning "USE_MODULE_UART not defined in system.h. Other modules won't be able to utilize this module."
+#endif
+
+#include "subsys.h"
+
+#ifdef USE_MODULE_TASK
+#include "task.h"
+#endif
+
+#ifdef USE_MODULE_BUFFER_PRINTF
+#include "buffer_printf.h"
+#endif
+
+#include "buffer.h"
+
+#include "char_receiver_list.h"
+
+/** NOTE:  This enumeration has to occur BEFORE the include for
+ * the hal_uart.h because hal_uart.h includes this uart.h file
+ * so that it can see this error enumeration.
+ */
+enum uart_errors {
+    BREAK_ERROR = 0,
+    PARITY_ERROR,
+    FRAMING_ERROR,
+    OVERRUN_ERROR,
+    NUM_UART_ERRORS
+};
+
+/**************************************
+ * Hardware Abstraction Layer Includes
+ *************************************/
+#include "hal_uart.h"
+
+/** Initialize UART module
+ *
+ * Example usage:
+ * @code
+ * UART_Init(UART0_CHANNEL);
+ * @endcode
+ *
+ * @param channel - The channel of UART to be used.  Macros for these should be defined in the
+ * HAL of the specific device.
+ */
+void UART_Init(uint8_t channel);
+
+/** @brief Change baud rate
+ *
+ * @param channel - The channel of the UART to have its baud rate changed.
+ * @param baud - The new baud rate.
+ */
+void UART_ReconfigureBaud(uint8_t channel, uint32_t baud);
+
+/** @brief Write a single byte to UART
+ * 
+ * Transmit a single byte over the UART channel
+ *
+ * @param channel - The UART channel to send the byte over
+ * @param c - the byte to send
+ */
+void UART_WriteByte(uint8_t channel, uint8_t c);
+
+/**
+ * @brief Write chunk of data to UART tx buffer
+ *
+ * @param channel - the UART channel to send on
+ * @param data - a pointer to the data to send
+ * @param length - the amount of data to send
+ * @return 0 = success, -1 = not enough room in buffer, -2 = Invalid channel
+ */
+int8_t UART_Write(uint8_t channel, uint8_t * data, uint16_t length);
+
+// this module could be used without buffer_printf if these functions are
+// not used. Flag added to make it easy to not require buffer_printf
+#ifdef _BUFFER_PRINTF_H_
+/**
+ * @brief print formatted string to UART
+ * 
+ * See @ref Push_printf() for string format details.
+ *
+ * Uses Push_vprintf() to send formatted string out UART buffer
+ *
+ * @param channel - the UART channel to send on
+ * @param str - formatted string
+ * @param ... - variable list of replacement data
+ */
+void UART_Printf(uint8_t channel, char * str, ...);
+
+/**
+ * @brief print formatted string to UART
+ * 
+ * See @ref Push_printf() for string format details.
+ *
+ * Use this function from another function which had a variable argument list input.
+ *
+ * @param channel - the UART channel to send on
+ * @param str - formatted string
+ * @param vars - variable list of replacement data
+ */
+void UART_vprintf(uint8_t channel, char * str, va_list vars);
+#endif
+
+/**
+ * @brief Returns 0 or num bytes to indicate if the UART is transmitting
+ *
+ * @param channel - UART channel to check
+ * @return 0 if not transmitting, number of bytes in buffer otherwise
+ */
+uint8_t UART_IsTransmitting(uint8_t channel);
+
+/**
+ * @brief Main UART routine which calls subscribers and handles errors
+ *
+ * If the task management module is used and USE_MODULE_TASK is defined then
+ * this function will automatically run via the task management system. Otherwise
+ * the user should call UART_Tick() frequently to handle incoming data.
+ */
+void UART_Tick(void);
+
+/**
+ * @brief register a character receiver for a UART channel to receive data
+ *
+ * The receiver function registered will be called by UART_Tick whenever a byte
+ * is received. The receiver function should have no return value and a char input.
+ *
+ * @param channel - UART channel to register receiver with
+ * @param fn - function pointer to receiver function: void ReceiverName(char c)
+ * @return -1 if channel is out of range, -2 if receiver list is full, 0 if successful
+ */
+int8_t UART_RegisterReceiver(uint8_t channel, charReceiver_t fn);
+
+/**
+ * @brief Unregister a receiver added by UART_RegisterReceiver()
+ *
+ * @param channel - UART channel to unregister receiver on
+ * @param fn - function pointer to receiver function: void ReceiverName(char c)
+ */
+void UART_UnregisterReceiver(uint8_t channel, charReceiver_t fn);
+
+/**
+ * @brief Register callback function to handle UART errors
+ *
+ * Will call the callback function when a UART error occurs.
+ * The callback should have no return value and a enum uart_errors input.
+ *
+ * @param channel - UART channel to tie the error callback function to
+ * @param callback - function pointer to callback function: void CallbackName(enum uart_errors error)
+ */
+void UART_RegisterErrorCallback(uint8_t channel, void(*callback)(enum uart_errors));
+
+/**
+ * @brief Register callback function to be called when/if the UART Tx buffer overflows
+ *
+ * Will be called when trying to transmit data and the Tx buffer is full.
+ * The callback should have no return value and no input.
+ *
+ * @param channel - UART channel to tie the tx overwrite callback to
+ * @param overwriteCallback - function pointer to callback function: void CallbackName(void)
+ */
+void UART_RegisterTxOverwriteCallback(uint8_t channel, void(*overwriteCallback)(void));
+
+/**
+ * @brief Register callback function to be called when/if the UART Rx buffer overflows
+ *
+ * Will be called when trying to transmit data and the Rx buffer is full.
+ * The callback should have no return value and no input.
+ *
+ * @param channel - UART channel to tie the Rx overwrite callback to
+ * @param overwriteCallback - function pointer to callback function: void CallbackName(void)
+ */
+void UART_RegisterRxOverwriteCallback(uint8_t channel, void(*overwriteCallback)(void));
+
+
+/**************************************
+ * Hardware Abstraction Layer Declarations
+ *************************************/
+
+ // In some cases the HAL will implement these with a #define which is why each is
+ // surrounded with a #ifndef / #endif
+ 
+/** @brief initialize UART channel at specified baud rate
+ *
+ * REQUIRED
+ *
+ * This function should:
+ * - Initialize the UART module for the channel specified
+ * - Map UART pins if needed
+ * - Configure UART interrupts
+ *
+ * @param channel - UART channel to initialize
+ * @param baud - integer baud rate (bits/s) to configure UART for
+ */
+ #ifndef hal_UART_Init
+void hal_UART_Init(uint8_t channel, uint32_t baud);
+#endif
+
+
+/** @brief change the baud rate for the UART channel
+ *
+ * @param channel - UART channel
+ * @param baud - integer baud rate (bits/s) to configure UART for
+ */
+#ifndef hal_UART_ReconfigureBaud
+void hal_UART_ReconfigureBaud(uint8_t channel, uint32_t baud);
+#endif
+
+/** @brief UART transmit interrupt handler
+ *
+ * REQUIRED to be called by HAL
+ *
+ * This function is implemented by the UART module (uart.c) but should be
+ * called by the ISR for the given UART channel.
+ *
+ * @param channel - channel the interrupt occurred on
+ */
+#ifndef UART_Tx_Handler
+void UART_Tx_Handler(uint8_t channel);
+#endif
+
+/** @brief UART receive interrupt handler
+ *
+ * REQUIRED to be called by HAL
+ *
+ * This function is implemented by the UART module (uart.c) but should be
+ * called by the ISR for the given UART channel.
+ *
+ * @param channel - channel the interrupt occurred on
+ */
+#ifndef UART_Rx_Handler
+void UART_Rx_Handler(uint8_t channel);
+#endif
+
+/** @brief UART error (interrupt) handler
+ *
+ * This function is implemented by the UART module (uart.c) but should be
+ * called by the error ISR for the given UART channel or when an error condition
+ * is detected by the HAL.
+ *
+ * @param channel - channel the interrupt occurred on
+ */
+#ifndef UART_Error_Handler
+void UART_Error_Handler(uint8_t channel, enum uart_errors error);
+#endif
+
+/** @brief Enable UART peripheral for given channel
+ */
+#ifndef hal_UART_Enable
+void hal_UART_Enable(uint8_t channel);
+#endif
+
+/** @brief Disable UART peripheral for given channel
+ */
+#ifndef hal_UART_Disable
+void hal_UART_Disable(uint8_t channel);
+#endif
+
+/** @brief return 0 if no space is available in the hardware UART tx buffer
+ *
+ * REQUIRED
+ */
+#ifndef hal_UART_SpaceAvailable
+uint8_t hal_UART_SpaceAvailable(uint8_t channel);
+#endif
+
+/** @brief return 0 if no data is available in the hardware UART rx buffer
+ *
+ * REQUIRED
+ */
+#ifndef hal_UART_DataAvailable
+uint8_t hal_UART_DataAvailable(uint8_t channel);
+#endif
+
+/** @brief clear the UART rx interrupt flags
+ */
+#ifndef hal_UART_ClearRxIF
+void hal_UART_ClearRxIF(uint8_t channel);
+#endif
+
+/** @brief clear the UART tx interrupt flags
+ */
+#ifndef hal_UART_ClearTxIF
+void hal_UART_ClearTxIF(uint8_t channel);
+#endif
+
+/** @brief enable the UART rx interrupt
+ */
+#ifndef hal_UART_EnableRxInterrupt
+void hal_UART_EnableRxInterrupt(uint8_t channel);
+#endif
+
+/** @brief enable the UART tx interrupt
+ *
+ * REQUIRED
+ */
+#ifndef hal_UART_EnableTxInterrupt
+void hal_UART_EnableTxInterrupt(uint8_t channel);
+#endif
+
+/** @brief disable the UART rx interrupt
+ */
+#ifndef hal_UART_DisableRxInterrupt
+void hal_UART_DisableRxInterrupt(uint8_t channel);
+#endif
+
+/** @brief disable the UART tx interrupt
+ *
+ * REQUIRED
+ */
+#ifndef hal_UART_DisableTxInterrupt
+void hal_UART_DisableTxInterrupt(uint8_t channel);
+#endif
+
+/** @brief Is UART rx interrupt enabled
+ */
+#ifndef hal_UART_RxInterruptEnabled
+uint8_t hal_UART_RxInterruptEnabled(uint8_t channel);
+#endif
+
+/** @brief Is UART tx interrupt enabled
+ *
+ * REQUIRED
+ */
+#ifndef hal_UART_TxInterruptEnabled
+uint8_t hal_UART_TxInterruptEnabled(uint8_t channel);
+#endif
+
+/** @brief Get character from UART hardware rx register
+ *
+ * REQUIRED
+ */
+#ifndef hal_UART_RxByte
+uint8_t hal_UART_RxByte(uint8_t channel);
+#endif
+
+/** @brief send character to UART hardware tx register
+ * 
+ * REQUIRED
+ */
+#ifndef hal_UART_TxByte
+void hal_UART_TxByte(uint8_t channel, uint8_t data);
+#endif
+
+/** @}*/
+#endif /* _UART_H_ */
