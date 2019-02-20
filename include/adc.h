2053179d3ea57109d0fbe754c012c1be7b2639cc
Michael Muhlbaier <muhlbaier@rowan.edu>
2019-02-19 21:12:02 -0500

    Initial commit of ADC module - no HAL support yet

---
+++ include/adc.h
@@ -0,0 +1,55 @@
+#ifndef _ADC_H_
+#define _ADC_H_
+/**
+ * @defgroup adc_module ADC Module
+ *
+ * Here is my description of what this module does and how to use it.
+ *
+ * @{
+ */
+ 
+/** @brief Initialize the ADC module
+ * 
+ * This is the longer description which uses more words to say initializes the ADC module.
+ *
+ * @author Muhlbaier
+ * @version 0.0
+ */ 
+void ADC_Init(void);
+
+/** @brief add a channel to the list of channels to measure, a period at which to measure it, and a callback
+ *
+ * @param channel ADC channel index base 0
+ * @param period Period between measurements in milliseconds
+ * @param callback Callback when measurements are complete. Can be void Function(uint16_t value) or void Function(uint16_t value, void * ptr)
+ * @param ptr Optional pointer to be passed to the callback function
+ */
+void ADC_AddChannel(uint8_t channel; uint16_t period, void(*callback)(uint16_t, void *), void * ptr);
+
+/**
+ * Function to be called from the ADC interrupt in hal_adc.c with the measurement
+ *
+ * @param value the ADC measurement
+ */
+void ADC_ProcessMeasurementFromISR(uint16_t value);
+
+/** @brief hardware abstaction layer ADC initialization
+ *
+ * Must be implemented for each MCU in hal_adc.c and configure the ADC for single measurements with
+ * interrupts enabled after completion of measurements.
+ */
+void hal_ADC_Init(void);
+
+/** @brief hardware abstaction layer start ADC measurement for a channel
+ *
+ * Must be implemented for each MCU in hal_adc.c and setup the ADC to sample the given channel, automatically
+ * start the conversion, and interrupt when the conversion is complete. A interrupt service routine also
+ * needs to be added to hal_adc.c which calls ADC_ProcessMeasurementFromISR() with the measured value.
+ */
+void hal_ADC_StartChannel(uint8_t channel);
+
+/** Maximum number of concurrent channels */
+#define ADC_MAX_CHANNELS 8
+
+ /** @} */
+#endif // _ADC_H_
---
+++ src/adc.c
@@ -0,0 +1,75 @@
+#include "adc.h"
+
+/** documentation for struct */
+struct adc_channel {
+	uint8_t channel; // channel index for the ADC
+	uint16_t value; ///< result of ADC measurement (documentation for struct member)
+	void(*callback)(uint16_t, void *); // callback function pointer
+	void * ptr; // optional pointer for the callback function
+};
+
+static struct adc_channel channels[ADC_MAX_CHANNELS];
+// number of channels that have been added via ADC_AddChannel()
+static uint8_t num_channels;
+
+#define ADC_QUEUE_SIZE 10
+static struct adc_channel * pending_measurements[ADC_QUEUE_SIZE];
+// start is the index of the first pending measurement when start != end and full is 0
+// end is the next available index to add a pending measurement
+// full is a binary flag to indicate if the circular queue is full or not
+static uint8_t start, end, full;
+
+void ADC_Init(void) {
+	num_channels = 0;
+	start = 0;
+	end = 0;
+	full = 0;
+	hal_ADC_Init(void);
+}
+
+void ADC_AddChannel(uint8_t channel; uint16_t period, void(*callback)(uint16_t, void *), void * ptr) {
+	if(num_channels < ADC_MAX_CHANNELS) {
+		channels[num_channels].channel = channel
+		channels[num_channels].callback = callback;
+		channels[num_channels].ptr = ptr;
+		Task_Schedule(QueueMeasurement, &channels[num_channels], period, period);
+		num_channels++;
+	}
+}
+
+static void QueueMeasurement(struct adc_channel * channel) {
+	// add measurement to the queue
+	if(!full) {
+		BlockInterrupts();
+		// if empty then start the ADC conversion
+		if(start == end) {
+			hal_ADC_StartChannel(channel->channel);
+		}
+		pending_measurements[end++] = channel;
+		if(end >= ADC_QUEUE_SIZE) end = 0;
+		if(end == start) full = 1;
+		RestoreInterrupts();
+	}
+}
+
+// must be interrupt safe
+void ADC_ProcessMeasurementFromISR(uint16_t value) {
+	pending_measurements[start]->value = value;
+	Task_Queue(CallCallback, pending_measurements[start]);
+	full = 0;
+	start++;
+	if(start >= ADC_QUEUE_SIZE) start = 0;
+	// if not empty then start the next conversion
+	if(start != end) {
+		hal_ADC_StartChannel(pending_measurements[start]->channel);
+	}
+}
+
+static void CallCallback(struct adc_channel * channel_struct) {
+	// if the pointer is set then call the callback with the value and the pointer
+	if(channel_struct->ptr) {
+		channel_struct->callback(channel_struct->value, channel_struct->ptr);
+	}else {
+		channel_struct->callback(channel_struct->value);
+	}
+}
