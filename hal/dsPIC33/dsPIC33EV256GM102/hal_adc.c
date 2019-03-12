4e95b77b60c57fd0415174279ccaebd05883821e
Nate Hoffman <nhoffman15@me.com>
2019-03-12 08:32:25 -0400

    Add comments on ADC configuration.  Add up to 64 different ADC channels selectable.

--- hal/dsPIC33/dsPIC33EV256GM102/hal_adc.c
+++ hal/dsPIC33/dsPIC33EV256GM102/hal_adc.c
@@ -3,35 +3,33 @@
 
 void hal_ADC_Init() {
     AD1CON1 = 0;
-    AD1CON1bits.AD12B = 1;
-    AD1CON1bits.SSRC = 7;
+    AD1CON1bits.AD12B = 1; // 12 bit
+    AD1CON1bits.SSRC = 7; // Internal counter ends sampling and starts convert
     
     AD1CON2 = 0;
     
     AD1CON3 = 0;
-    AD1CON3bits.SAMC = 0x1F;
-    AD1CON3bits.ADRC = 1;
+    AD1CON3bits.ADRC = 1; // Internal conversion clock select
+    AD1CON3bits.SAMC = 0x1F; // Auto sample time 31 Tad
     
     AD1CON4 = 0;
-    
-    AD1CHS0 = 0;
     
     AD1CSSH = 0;
     AD1CSSL = 0;
     
-    IPC3bits.AD1IP = 2;
-    IEC0bits.AD1IE = 1;
-    IFS0bits.AD1IF = 0;
+    IPC3bits.AD1IP = 2; // Interrupt priority
+    IFS0bits.AD1IF = 0; // Interrupt clear
+    IEC0bits.AD1IE = 1; // Interrupt enable
     
-    AD1CON1bits.ADON = 1;
+    AD1CON1bits.ADON = 1; // Turn on ADC
 }
 
 void hal_ADC_StartChannel(uint8_t channel) {
-    AD1CON1bits.SAMP = 1;
+    AD1CHS0 = channel & 0x3F; // Only 64 channels available
+    AD1CON1bits.SAMP = 1; // Start sample and conversion
 }
 
 void __attribute__((interrupt, auto_psv)) _AD1Interrupt() {
-    IFS0bits.AD1IF = 0;
-    uint16_t value = ADC1BUF0;
-    ADC_ProcessMeasurementFromISR(value);
+    IFS0bits.AD1IF = 0; // Interrupt clear
+    ADC_ProcessMeasurementFromISR(ADC1BUF0);
 }
