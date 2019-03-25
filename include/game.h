0918b9a6c230bb7272886f3cb12d4af5a1165d20
Alex Marino <marinoa2@students.rowan.edu>
2019-03-25 16:38:58 -0400

    Adds functions to game.c and game.h to spawn buildings and check collisions between them and any moving char_object_t.

--- include/game.h
+++ include/game.h
@@ -83,6 +83,37 @@ typedef struct {
     uint8_t skill; ///< 0-9 skill level of player
 }player_info_t;
 
+/**@struct building_t
+ *
+ * @brief Building info structure
+ *
+ * Holds building origin, dimensions, and door location
+ *
+ * @warning Y values start from the top. Make max_y the larger value but recall that it will be the "bottom"
+ */
+typedef struct building_t{
+    char x; //distance from left of screen to close wall
+    char y; //distance from top of screen to close wall
+    char max_x; //distance from left of screen to far wall
+    char max_y; //distance from top of screen to far wall
+    char dx; //x coordinate of door
+    char dy; //y coordinate of door
+}building_t;
+
+/** @brief Enum to store collision type for use with building_t
+ *
+ * Use these values to determine if a collision occurs or if the object is in a doorway
+ * If DOOR is detected, user should produce 2 movements so that the object moves through the door,
+ * as the object could move upwards or downwards through the door if it stays in the doorway
+ */
+enum check_collision
+{
+    NO_COLLISION = 0,
+    COLLISION = 1,
+    DOOR = 2
+};
+
+
 #ifndef TRANSPARENT_CHAR
 #define TRANSPARENT_CHAR '`' ///< transparent character for drawing tiles
 #endif
@@ -399,6 +430,45 @@ void Game_Bell(void);
  * module can allow users to play again.
  */
 void Game_GameOver(void);
+
+/** @brief Function to draw buildings with doors
+ *
+ * @param building is a building_t that stores building parameters
+ */
+static void Game_DrawBuilding(building_t building);
+
+/** @brief Dectects collision during downward movement
+ *
+ * @param building is a building_t
+ *
+ * @param player is any char_object_t whose position might conflict with a building
+ */
+static enum check_collision Game_CollisionDown(building_t building, char_object_t player);
+
+/** @brief Dectects collision during upward movement
+ *
+ * @param building is a building_t
+ *
+ * @param player is any char_object_t whose position might conflict with a building
+ */
+static enum check_collision Game_CollisionUp(building_t building, char_object_t player);
+
+/** @brief Dectects collision during leftward movement
+ *
+ * @param building is a building_t
+ *
+ * @param player is any char_object_t whose position might conflict with a building
+ */
+static enum check_collision Game_CollisionLeft(building_t building, char_object_t player);
+
+/** @brief Dectects collision during rightward movement
+ *
+ * @param building is a building_t
+ *
+ * @param player is any char_object_t whose position might conflict with a building
+ */
+static enum check_collision Game_CollisionRight(building_t building, char_object_t player);
+
 
 #ifdef USE_MODULE_GAME_NFR24
 #include "nrf24.h"
