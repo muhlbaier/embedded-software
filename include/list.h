6f2822dd806290abe366c1ed904209fa2328ae06
okunj4 <36168902+okunj4@users.noreply.github.com>
2019-03-26 17:46:36 -0400

    Update list.h
    Updated the statements to reflect what you said.

--- include/list.h
+++ include/list.h
@@ -255,7 +255,7 @@ void List_RemoveLast(list_t* list);
  * Removes an element of list
  * @param list a pointer to a list_t
  * @param identifier for the item to remove in the list.
- * @return a void pointer to the item found using the identifier, therefore pointing the item to be removed.
+ * @return is a pointer that removes the item and returns no value.
  *
  * @warning This method does not return the items that were removed.
  */
@@ -274,7 +274,7 @@ void List_RemoveAll(list_t* list);
  * Unlinks an item of list.  If a valid pointer is not found an error code may be given
  * @param lists a pointer to a list_t
  * @param identifier for the item to unlink in the list.
- * @return a void pointer to the item found using the identifier, therefore pointing the item to be unlinked.
+ * @return is a pointer that unlinks the item and returns no value.
  *
  * @warning This method does not return the items that were unlinked.
  */
