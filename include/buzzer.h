fd950c1e4444efcf9efa679670ef7c147d056773
Nikola Kosaric <nikolakosaric@gmail.com>
2019-04-02 19:20:23 -0400

    Added further documentation regarding the buzzer module

--- include/buzzer.h
+++ include/buzzer.h
@@ -8,6 +8,19 @@
 
 /**
  * @defgroup buzzer_module Buzzer Module
+ * 
+ * The following module utilizes the task manager and timing module
+ * in order to inact a buzzer to produce a sound at a determined frequency.
+ * The timing module is used to control the frequency of the sound produced
+ * and the task module is used to control the length of time the buzzer will 
+ * sound.
+ *
+ * In oder to utilize this module, first, the Buzzer_Init must be called.
+ * Then, the Buzzer_Sound function must be called in order to determine the
+ * frequency at which the buzzer will sound and the length of time the buzzer
+ * will sound. The channel used for the buzzer will be determined in the 
+ * hardware abstraction layer.
+ * 
  * @{
  */
 
