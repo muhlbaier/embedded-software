9b33831bee43f43cf27b987a2eec53580859f774
Nate Hoffman <nhoffman15@me.com>
2019-04-09 18:39:39 -0400

    Add buzzer source code.

---
+++ src/buzzer.c
@@ -0,0 +1,11 @@
+#include "task.h"
+#include "buzzer.h"
+
+void Buzzer_Init(void){
+    hal_Buzzer_Init();
+}
+
+void Buzzer_Sound(uint16_t frequency, uint16_t timeout){
+    hal_Start_Buzzer(frequency);
+    Task_Schedule((task_t) hal_Stop_Buzzer, 0, timeout, 0);
+}
